efb38082e7d2a62b9e5ba9510fc55632fb3462d9
Yan <podoyo23@gmail.com>
2025-03-27 23:20:01 +0800

    HomeView新增bars-solid + 登入按鈕 + 新增未登入前文章版型

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@ import { RouterLink, RouterView } from "vue-router"
 
 <template>
 	<div>
-		<nav>
-			<RouterLink to="/">Home</RouterLink>
+		<nav class="mb-4 bg-white shadow p-5 flex justify-between">
+			<RouterLink to="/"><img class="w-5 h-auto text-white" src="../src/assets/bars-solid .png" alt="" /></RouterLink>
+			<RouterLink to="/login" class="bg-sky-600 pl-4 pr-4 pt-2 pb-2 rounded text-white">登入</RouterLink>
 		</nav>
 	</div>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,25 @@
 <script setup></script>
 
 <template>
-	<main>
-		<TheWelcome />
-	</main>
+	<div class="grid grid-cols-[3fr_1fr] p-5 gap-4">
+		<div class="bg-slate-300 rounded-2xl h-auto p-5">
+			<div>
+				<h2 class="p-5 mb-5 text-white text-xl">快來新增新的文章吧!</h2>
+			</div>
+			<div class="bg-white rounded-2xl h-auto m-4 p-5">
+				<div class="flex justify-between p-2">
+					<p>我是文章區</p>
+					<button class="font-extrabold">V</button>
+				</div>
+			</div>
+		</div>
+		<div class="bg-slate-300 rounded-2xl h-auto p-5">空白區</div>
+	</div>
 </template>
+
+<style>
+h2 {
+	font-weight: 700;
+	text-shadow: 0px 0px 2px rgba(0, 0, 0, 0.7);
+}
+</style>
