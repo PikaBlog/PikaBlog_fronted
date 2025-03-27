a0c4385632f81ba76737916b5f68df5d60bb698d
Yan <podoyo23@gmail.com>
2025-03-27 23:44:47 +0800

    修正bars-solid 至Sidebar組件，並新增Vue檔案 Sidebar 組件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router"
+import Sidebar from "./components/Sidebar.vue"
+Sidebar
 </script>
 
 <template>
 	<div>
+		<!-- 側邊欄 -->
+		<Sidebar />
 		<nav class="mb-4 bg-white shadow p-5 flex justify-between">
-			<RouterLink to="/"><img class="w-5 h-auto text-white" src="../src/assets/bars-solid .png" alt="" /></RouterLink>
-			<RouterLink to="/login" class="bg-sky-600 pl-4 pr-4 pt-2 pb-2 rounded text-white">登入</RouterLink>
+			<RouterLink to="/">Home</RouterLink>
+			<!-- <button><img class="w-5 h-auto text-white" src="../src/assets/bars-solid .png" alt="" /></button> -->
+			<RouterLink to="/login" class="border-sky-500 border-2 px-4 py-2 rounded text-white bg-sky-500 hover:bg-sky-600 active:bg-white active:text-sky-500">
+				登入
+			</RouterLink>
 		</nav>
 	</div>
 
-	<RouterView />
+	<!-- <RouterView /> -->
 </template>
 
 <style scoped></style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+	<button @click="toggleSidebar" class="fixed top-5 left-5 z-50 p-2 bg-white rounded">
+		<img class="w-6 h-auto" src="../assets/bars-solid .png" alt="menu" />
+	</button>
+	<!-- 漢堡選單按鈕 -->
+</template>
+
+<style scoped></style>
